0c89a38b2d102cb4ca1402e8f9ce51e462459eb3
Benjamin Schneider <benjaminschneider@Digital-Festival.local>
2020-08-09 17:06:26 +0200

    New books page

---
+++ build-books.js
@@ -0,0 +1,41 @@
+'use strict';
+const fs = require('fs');
+
+let template = (title, img, url) => {
+    return `
+    <div class="book">
+        <a href="${url}" target="_blank" title='${title}'>
+            <img src="img/${img}" alt='${title}'/>
+        </a>
+    </div>
+    `;
+}
+
+let data = fs.readFileSync("./books.json");
+let books = JSON.parse(data);
+
+// Sort by Last Name (from title strings)
+books.sort((a, b) => {
+    let aK = a.title;
+    let bK = b.title;
+    if (aK < bK) return -1;
+    if (aK > bK) return 1;
+    return 0;
+})
+
+let out = `<div class="books-container">
+`;
+books.forEach(element => {
+    out += template(element.title, element.image, element.link);
+});
+
+// Fill flex grid so the spacing works...
+for (let index = 0; index < (3 - (books.length % 3)) && index < 3; index++) {
+    out += `
+        <div class="book-filler"></div>
+    `;
+}
+
+out += `</div>`;
+
+console.log(out);
